a05260cfddbb4b2d445571c18b8c1e9d4995c3df
masaro.matsumoto <m.masaro.h8@gmail.com>
2020-09-27 11:29:24 +0900

    Modify ContentstitleServlet.java

--- src/models/ContentTitle.java
+++ src/models/ContentTitle.java
@@ -6,27 +6,80 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
 @Table(name = "contenttitle")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllEmployees",
+            query = "SELECT e FROM ContentTitle AS e ORDER BY e.id DESC"
+            ),
+    @NamedQuery(
+            name = "getEmployeesCount",
+            query = "SELECT COUNT(e) FROM ContentTitle AS e"
+            ),
+    @NamedQuery(
+            name = "checkRegisteredCode",
+            query = "SELECT COUNT(e) FROM ContentTitle AS e WHERE e.code = :code"
+            ),
+    @NamedQuery(
+            name = "checkLoginCodeAndPassword",
+            query = "SELECT e FROM ContentTitle AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+            )
+})
 @Entity
 public class ContentTitle {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)//id リソース内での連番   数値型
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(name = "password", length = 64, nullable = false)//password システムへのログインパスワード 文字列型
+
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", length = 64, nullable = false)
     private String password;
-    @Column(name = "created_at", nullable = false)//created_at  登録日時    日時型
+
+    @Column(name = "admin_flag", nullable = false)
+    private Integer admin_flag;
+
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
-    @Column(name = "updated_at", nullable = false)//
+
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
+
+    @Column(name = "delete_flag", nullable = false)
+    private Integer delete_flag;
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -35,6 +88,13 @@ public class ContentTitle {
         this.password = password;
     }
 
+    public Integer getAdmin_flag() {
+        return admin_flag;
+    }
+
+    public void setAdmin_flag(Integer admin_flag) {
+        this.admin_flag = admin_flag;
+    }
 
     public Timestamp getCreated_at() {
         return created_at;
@@ -52,4 +112,11 @@ public class ContentTitle {
         this.updated_at = updated_at;
     }
 
+    public Integer getDelete_flag() {
+        return delete_flag;
+    }
+
+    public void setDelete_flag(Integer delete_flag) {
+        this.delete_flag = delete_flag;
+    }
 }
