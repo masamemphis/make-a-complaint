1204add5127c8387e6f3891c1c7904e7aa08fca5
masaro.matsumoto <m.masaro.h8@gmail.com>
2020-09-30 17:09:27 +0900

    Modyfi ContentsTitle index

---
+++ src/controllers/contentstitle/ContentsTitleCreateServlet.java
@@ -0,0 +1,79 @@
+package controllers.contentstitle;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.ContentTitle;
+import models.validation.ContentsValidation;
+import utils.DBUtil;
+import utils.EncryptUtil;
+
+/**
+ * Servlet implementation class ContentsTitleCreateServlet
+ */
+@WebServlet("/contetstitle/create")
+public class ContentsTitleCreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ContentsTitleCreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            ContentTitle e = new ContentTitle();
+
+            e.setCode(request.getParameter("code"));
+            e.setName(request.getParameter("name"));
+            e.setPassword(
+                    EncryptUtil.getPasswordEncrypt(
+                            request.getParameter("password"),
+                            (String)this.getServletContext().getAttribute("pepper")
+                            )
+                    );
+            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            e.setCreated_at(currentTime);
+            e.setUpdated_at(currentTime);
+            e.setDelete_flag(0);
+
+            List<String> errors = ContentsValidation.validate(e, true, true);
+            if(errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("user", e);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/contentstitle/new.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.persist(e);
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+
+                response.sendRedirect(request.getContextPath() + "/contentstitle/index");
+            }
+        }
+    }
+
+}
--- src/controllers/contentstitle/ContentstitleServlet.java
+++ src/controllers/contentstitle/ContentstitleServlet.java
@@ -1,6 +1,7 @@
 package controllers.contentstitle;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,23 +12,23 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class ContentstitleServlet
  */
 @WebServlet("/contentstitle")
-public class ContentstitleServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+public class ContentsTitleServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ContentstitleServlet() {
+    public ContentsTitleServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
 
 }
